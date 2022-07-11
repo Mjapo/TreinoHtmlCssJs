c2e3fee97a68b5bda1d65235c2341c658821208c
Marcelo ishida <96395400+Mjapo@users.noreply.github.com>
2022-07-11 15:11:18 -0300

    Add files via upload

---
+++ html/style.js
@@ -0,0 +1,60 @@
+// usando console.log 
+
+console.log("hello word");
+
+// usando hello word com white 
+
+document.write("hello word!!!");
+
+// nomeando 
+
+document.getElementsByTagName('two')[0].innerHTML = "Hello World"
+
+// em forma de variavel 
+
+var footer = marcelo;
+footer = 'hello word!!!!!';
+
+// em forma de objeto 
+argumento [hello]
+argumento1 [word]
+
+argumento2 = argumento + argumento1
+
+//alert 
+
+alert(" Hello word");
+
+// em forma de funcao 
+
+function minhaFuncao(Object){
+    Object.make = "hello word"
+}
+
+
+// variaveis 
+
+ var x = "hello";  {
+    var x = "word";
+
+
+ }
+
+ console.log(x)
+
+
+ // imprimi saida de hello word
+
+    print("hello word !!!")
+
+
+
+    //   imprimi  hello word  !! 
+
+    public class HelloWorld{
+        public static void main(String[] args){
+            System.out.println("Hello word!!");
+
+
+        }
+    }
